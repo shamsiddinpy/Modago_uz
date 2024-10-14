0bbc85f39ce8c1786ab459bf04ef315628fbdeae
shamsiddindaminov <shamsiddinpython215@gmail.com>
2024-10-14 18:45:41 +0500

    modago uz

--- apps/shops/urls.py
+++ apps/shops/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
 from shops.views.base import BasedTemplateView
-from shops.views.select_shop import SelectShopTemplateView
+from shops.views.select_shop import WizardSelectShopTemplateView
 
 urlpatterns = [
     path('', BasedTemplateView.as_view(), name='shop'),
-    path('select-shop', SelectShopTemplateView.as_view(), name='select_shop'),
+    path('select-shop', WizardSelectShopTemplateView.as_view(), name='select_shop'),
 ]
--- apps/shops/views/select_shop.py
+++ apps/shops/views/select_shop.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class SelectShopTemplateView(TemplateView):
     template_name = 'apps/shops/select-shop.html'
+
+
+class WizardSelectShopTemplateView(TemplateView):
+    template_name = 'apps/shops/wizard.html'
